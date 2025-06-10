a88bd7e6317f115954d87d7875c7ce0461595aee
Shubham Dubey <shubhamdubey05092001@gmail.com>
2025-06-10 17:19:51 +0530

    Memoization

--- DynamicProgramming/KnapsackProblem/KnapSack.java
+++ DynamicProgramming/KnapsackProblem/KnapSack.java
@@ -1,19 +1,38 @@
 package DynamicProgramming.KnapsackProblem;
 
 public class KnapSack {
-   public static int solve(int idx, int w, int[] val, int[] wt) {
+   public static int solve(int idx, int w, int[] val, int[] wt, int[][] dp) {
+        if (idx == 0) {
+            if (wt[0] <= w) return val[0];
+            else return 0;
+        }
+
+        if (dp[idx][w] != -1) return dp[idx][w];
+
+        // Not take case
+        int notTake = solve(idx - 1, w, val, wt, dp);
+
+        // Take case
+        int take = Integer.MIN_VALUE;
+        if (wt[idx] <= w) {
+            take = val[idx] + solve(idx - 1, w - wt[idx], val, wt, dp);
+        }
+
+        return dp[idx][w] = Math.max(take, notTake);
+    }
+   public static int solve1(int idx, int w, int[] val, int[] wt) {
         if (idx == 0) {
             if (wt[0] <= w) return val[0];
             else return 0;
         }
 
         // Not take case
-        int notTake = solve(idx - 1, w, val, wt);
+        int notTake = solve1(idx - 1, w, val, wt);
 
         // Take case
         int take = Integer.MIN_VALUE;
         if (wt[idx] <= w) {
-            take = val[idx] + solve(idx - 1, w - wt[idx], val, wt);
+            take = val[idx] + solve1(idx - 1, w - wt[idx], val, wt);
         }
 
         return  Math.max(take, notTake);
@@ -23,7 +42,16 @@ public class KnapSack {
     int [] wt =  {5, 4, 2, 3};
     int w = 5;
     int n = val.length;
-    System.out.println(solve(n-1, w, val, wt));
+    int[][] dp = new int[n][w+ 1];
+
+        // Initialize DP array with -1
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= w; j++) {
+                dp[i][j] = -1;
+            }
+        }
+    System.out.println(solve1(n-1, w, val, wt));
+    System.out.println(solve(n-1, w, val, wt, dp));
 
   }
 }
