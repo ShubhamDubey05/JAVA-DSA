fb33376b6830124c2c3f31399201f24dce40c817
Shubham Dubey <shubhamdubey05092001@gmail.com>
2025-06-11 23:22:39 +0530

    Tabulation

--- DynamicProgramming/KnapsackProblem/KnapSack.java
+++ DynamicProgramming/KnapsackProblem/KnapSack.java
@@ -1,6 +1,37 @@
 package DynamicProgramming.KnapsackProblem;
 
 public class KnapSack {
+     public static int solve2(int [] val , int [] wt, int cap){
+          int n  = val.length;
+          
+
+          int [][] dp = new int [n+1][cap+1];
+          for(int i =  1; i<dp.length; i++){
+            for(int j =1; j<dp[0].length; j++){
+                // yadi batting nhi krta hai 
+                dp[i][j] = dp[i-1][j];
+
+                // yadi batting krta hai
+                if( j>=wt[i-1]){
+                    int rem  =  j - wt[i-1];
+                    if(dp[i-1][rem] + val[ i-1] > dp[i-1][j]){
+                        dp[i][j] = dp[i-1][rem] + val[ i-1];
+                    }
+                    else{
+                       dp[i][j] = dp[i-1][j]; 
+                    }
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+          }
+
+
+        return dp[n][cap];
+     }
+
+
    public static int solve(int idx, int w, int[] val, int[] wt, int[][] dp) {
         if (idx == 0) {
             if (wt[0] <= w) return val[0];
@@ -52,6 +83,7 @@ public class KnapSack {
         }
     System.out.println(solve1(n-1, w, val, wt));
     System.out.println(solve(n-1, w, val, wt, dp));
+    System.out.println(solve2(val, wt, w));
 
   }
 }
