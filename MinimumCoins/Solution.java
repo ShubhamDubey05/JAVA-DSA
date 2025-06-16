2b7b79840e1525c490c46575b6d451d0d0209864
Shubham Dubey <shubhamdubey05092001@gmail.com>
2025-06-16 20:58:38 +0530

    Tabulation

--- MinimumCoins/Solution.java
+++ MinimumCoins/Solution.java
@@ -17,6 +17,32 @@ public class Solution {
 
     return dp[idx][tar] = Math.min(take, notake);
   }
+ public static int solve2(int[] nums, int tar) {
+    int n = nums.length;
+    int[][] dp = new int[n][tar + 1];
+
+    
+    for (int i = 0; i < n; i++) dp[i][0] = 0;
+
+  
+    for (int j = 1; j <= tar; j++) {
+        if (j % nums[0] == 0)
+            dp[0][j] = j / nums[0];
+        else
+            dp[0][j] = (int) 1e9; 
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j <= tar; j++) {
+            int notTake = dp[i - 1][j];
+            int take = (j >= nums[i]) ? 1 + dp[i][j - nums[i]] : (int) 1e9;
+            dp[i][j] = Math.min(take, notTake);
+        }
+    }
+
+    return dp[n - 1][tar] >= (int) 1e9 ? -1 : dp[n - 1][tar];
+}
+
 
   public static void main(String[] args) {
     int[] coins = {2, 3, 5};
@@ -31,5 +57,6 @@ public class Solution {
     int ans = solve1(n - 1, tar, coins, dp);
     if (ans == Integer.MAX_VALUE) System.out.println(-1);
     else System.out.println(ans);
+    System.out.println(solve2(coins, tar));
   }
 }
